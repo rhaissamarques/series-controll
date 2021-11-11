2694715e642e1761f42c88d89d530ed6ae048497
rhaissamarques <rhaissa.marques@gmail.com>
2021-11-11 12:31:09 -0300

    criando  um arquivo separado para exibir a mensagem de erro, logar com email e senha corretos, caso contrário, exibir mensagem de erro.

---
+++ controle-series/resources/views/entrar/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layout')
+
+@section('cabecalho')
+Entrar
+@endsection
+
+@section('conteudo')
+
+@include('erros', ['errors' => $errors])
+
+<form method="post">
+    @csrf
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" require min="1" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="password">Senha</label>
+        <input type="password" name="password" id="password" require min="1" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary mt-3">
+        Entrar
+    </button>
+    <a href="#" class="btn btn-secondary mt-3">
+        Registrar-se
+    </a>
+</form>
+@endsection
--- controle-series/resources/views/series/create.blade.php
+++ controle-series/resources/views/series/create.blade.php
@@ -5,15 +5,9 @@ Adicionar série
 @endsection
 
 @section('conteudo')
-@if ($errors->any())
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
+
+@include('erros', ['errors' => $errors])
+
 <form method="post">
     @csrf
     <div class="row">
