20b8fa9e31170a34e5c57e16debc404e1ca5b1ca
rhaissamarques <rhaissa.marques@gmail.com>
2021-11-11 16:35:33 -0300

    fazendo testes

--- controle-series/tests/Feature/CriadorDeSerieTest.php
+++ controle-series/tests/Feature/CriadorDeSerieTest.php
@@ -2,21 +2,24 @@
 
 namespace Tests\Feature;
 
+use App\Services\CriadorDeSerie;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CriadorDeSerieTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function testCriarSerie()
+    {
+        $criadorDeSerie = new CriadorDeSerie();
+        $nomeSerie = 'Nome de teste';
+        $serieCriada = $criadorDeSerie->criarSerie($nomeSerie, 1, 1);
+
+        $this->assertInstanceOf(Serie::class, $serieCriada);
+        $this->assertDatabaseHas('series', ['nome' => $nomeSerie]);
+        $this->assertDatabaseHas('temporadas', ['serie_id' => $serieCriada->id, 'numero' => 1]);
+        $this->assertDatabaseHas('episodios', ['numero' => 1]);
     }
 }
